b77b35cae32d238d7faf06fd4462aff891652577
Yuchen Liu <lyc990312@hotmail.com>
2024-06-24 19:37:47 +1000

    Add the input area component to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
+import InputArea from "./components/InputArea/InputArea";
+
 function App() {
+
+  const [todos, setTodos] = useState<string[]>([]);
+
   return (
     <>
-      
+      <InputArea setTodos={setTodos} />
     </>
   )
 }
